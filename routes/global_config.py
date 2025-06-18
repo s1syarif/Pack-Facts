1cec83d52f4263cf96a72432eb24f01f8f99f498
s1syarif <syarif.22046@mhs.unesa.ac.id>
2025-06-18 15:23:41 +0700

    api

--- routes/global_config.py
+++ routes/global_config.py
@@ -1,2 +1,2 @@
 import os
-BASE_API_URL = os.environ.get("BASE_API_URL", "https://5336-2001-448a-11b0-3e35-e546-3a80-7b33-7668.ngrok-free.app")
+BASE_API_URL = os.environ.get("BASE_API_URL", "https://ml2-api-797618185896.us-central1.run.app")
