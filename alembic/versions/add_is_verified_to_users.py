00e5d80cfc17bce560fb852e6101ee93a4cff568
s1syarif <syarif.22046@mhs.unesa.ac.id>
2025-07-22 03:51:32 +0700

    v

--- alembic/versions/add_is_verified_to_users.py
+++ alembic/versions/add_is_verified_to_users.py
@@ -3,7 +3,7 @@ Add is_verified field to users table
 
 Revision ID: add_is_verified_to_users
 Revises: add_timezone_to_user
-Create Date: 2025-07-22
+Create Date: 2025-07-222
 """
 from alembic import op
 import sqlalchemy as sa
@@ -13,7 +13,9 @@ down_revision = 'add_timezone_to_user'
 branch_labels = None
 depends_on = None
 
+
 def upgrade():
+    op.add_column('users', sa.Column('is_verified', sa.Boolean(), nullable=False, server_default=sa.text('0')))
     op.add_column('users', sa.Column('is_verified', sa.Boolean(), nullable=False, server_default='0'))
 
 def downgrade():
